aeaf4234ab99c55fd9b76036a2648db4d0e1873a
kifen <kifenraph@gmail.com>
2020-09-13 15:41:34 +0100

    implement mintBatch function

--- contracts/MinerCards.sol
+++ contracts/MinerCards.sol
@@ -32,26 +32,58 @@ contract MinerCards is ERC1155 {
     }
 
     /**
-     * @dev Creates `amount` tokens of token type `id`, and assigns them to `account`.
+     * @dev Creates `amount` tokens of token type `id`, and assigns them to `_account`.
      * It updates the token supply for the token type.
      * Emits a {TransferSingle} event.
      *
-     * - `_to`           cannot be the zero address.
+     * - `_account`      cannot be the zero address.
      * - `_id`          must be between 0 and 4.
      * - `_quantity`    quantity of tokens to mint.
      */
     function mint(
-        address _to,
+        address _account,
         uint256 _id,
         uint256 _quantity
     ) public onlyOwner {
         require(_id >= 0 && _id <= 4, "MinerCards: Invalid Token Type.");
 
-        _mint(_to, _id, _quantity, "");
+        _mint(_account, _id, _quantity, "");
         tokenSupply[_id] = tokenSupply[_id].add(_quantity);
     }
 
     function getowner() public view returns (address) {
         return owner;
+    }
+
+    /**
+     * @dev mints a batch ot tokens of different types and assigns them to `_account`
+     *
+     * Requirements:
+     *
+     * - `ids` and `amounts` must have the same length.
+     * - If `_account` refers to a smart contract, it must implement {IERC1155Receiver-onERC1155BatchReceived} and return the
+     * acceptance magic value.
+     */
+    function mintBatch(
+        address _account,
+        uint256[] memory _ids,
+        uint256[] memory _amounts
+    ) public onlyOwner {
+        for (uint256 i = 0; i < _ids.length; i++) {
+            require(
+                checkID(_ids[i]) != false,
+                "MinerCards.mintBatch: Invalid Token Type."
+            );
+        }
+        _mintBatch(_account, _ids, _amounts, "");
+
+        for (uint256 i = 0; i < _amounts.length; i++) {
+            tokenSupply[_ids[i]] = tokenSupply[_ids[i]].add(_amounts[i]);
+        }
+    }
+
+    // checks if `_id` is a valid token type
+    function checkID(uint256 _id) internal returns (bool) {
+        return (_id >= 0 && _id <= 4);
     }
 }
