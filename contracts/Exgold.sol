44df206456f55daf04c36ec5c16d908e98ba54a4
kifen <kifenraph@gmail.com>
2020-09-27 08:44:24 +0100

    add vars name, symbol, totalsupply to exgold

--- contracts/Exgold.sol
+++ contracts/Exgold.sol
@@ -6,6 +6,11 @@ pragma solidity ^0.6.0;
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20.sol";
 
 contract Exgold is ERC20UpgradeSafe {
+    string private _name;
+    string private _symbol;
+    uint256 private constant _totalSupply = 5000000;
+    uint256 private constant _decimals = 6;
+
     /**
      * @dev  Initializer function (replaces constructor)
      * Sets the values for {name} and {symbol}, initializes {decimals} using {_setupDecimals}
@@ -13,13 +18,15 @@ contract Exgold is ERC20UpgradeSafe {
      * All three of these values are immutable: they can only be set once during
      * construction.
      */
-    function initialize(string memory _name, string memory _symbol)
+    function initialize(string memory name, string memory symbol)
         public
         initializer
     {
+        _name = name;
+        _symbol = symbol;
         __ERC20_init(_name, _symbol);
         _setupDecimals(6);
-        _mint(msg.sender, 5000000 * (10**uint256(decimals())));
+        _mint(msg.sender, _totalSupply * (10**uint256(_decimals)));
     }
 
     /**
